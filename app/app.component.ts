724817ca960e3970d26b51e9348226d8ada90153
Thuan Vo <vothanhthuan7990@gmail.com>
2017-08-01 22:02:12 +0700

    First HTTP Call

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,53 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { User } from './shared/models/user';
 
 @Component({
   selector: 'my-app',
   template: `
-    <header>
-      <nav class="navbar navbar-inverse">
-        <div class="navbar-header">
-          <a href="/" class="navbar-brand">My Angular 2 App!</a>
-        </div>
-      </nav>
-    </header>
-
-    <main>
-      <div class="row">
-
-        <div class="col-sm-4">
-          <div *ngIf="users">
-            <ul class="list-group users-list">
-              <li class="list-group-item" *ngFor="let user of users">
-                {{ user.name }} ({{ user.username }})
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div class="col-sm-8">
-          <div class="jumbotron">
-            <h1>Welcome to Our App!</h1>
-            <p>{{ message }}</p>
-          </div>
-        </div>
-
+    <div class="jumbotron">
+      <h1>Welcome to Our App!</h1>
+    </div>
+    <div *ngIf="users">
+      <div *ngFor="let user of users">
+        <h2>{{ user.first_name }}</h2>
       </div>
-
-    </main>
-
-    <footer class="text-center">
-      Copyright &copy; 2017
-    </footer>
-  `,
-  styles: [`
-    .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }
-  `]
+    </div>
+  `
 })
-export class AppComponent {
-  message = 'Hello!';
-  users = [
-    {id: 7, name: 'Thuan', username: 'thuanvo' },
-    {id: 8, name: 'Wayne', username: 'brucewayne' },
-    {id: 9, name: 'James', username: 'jamesbond' }
-  ];
+export class AppComponent implements OnInit {
+  users: User[];
+
+  constructor(private http: Http) {}
+
+  ngOnInit() {
+    // grab users
+    this.http.get('https://reqres.in/api/users')
+      .subscribe(data => {
+        console.log(data.json());
+        this.users = data.json().data;
+      });
+  }
+
 }
--- dist/app.component.js
+++ dist/app.component.js
@@ -5,23 +5,31 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var http_1 = require("@angular/http");
 var AppComponent = (function () {
-    function AppComponent() {
-        this.message = 'Hello!';
-        this.users = [
-            { id: 7, name: 'Thuan', username: 'thuanvo' },
-            { id: 8, name: 'Wayne', username: 'brucewayne' },
-            { id: 9, name: 'James', username: 'jamesbond' }
-        ];
+    function AppComponent(http) {
+        this.http = http;
     }
+    AppComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        // grab users
+        this.http.get('https://reqres.in/api/users')
+            .subscribe(function (data) {
+            console.log(data.json());
+            _this.users = data.json().data;
+        });
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n    <header>\n      <nav class=\"navbar navbar-inverse\">\n        <div class=\"navbar-header\">\n          <a href=\"/\" class=\"navbar-brand\">My Angular 2 App!</a>\n        </div>\n      </nav>\n    </header>\n\n    <main>\n      <div class=\"row\">\n\n        <div class=\"col-sm-4\">\n          <div *ngIf=\"users\">\n            <ul class=\"list-group users-list\">\n              <li class=\"list-group-item\" *ngFor=\"let user of users\">\n                {{ user.name }} ({{ user.username }})\n              </li>\n            </ul>\n          </div>\n        </div>\n\n        <div class=\"col-sm-8\">\n          <div class=\"jumbotron\">\n            <h1>Welcome to Our App!</h1>\n            <p>{{ message }}</p>\n          </div>\n        </div>\n\n      </div>\n\n    </main>\n\n    <footer class=\"text-center\">\n      Copyright &copy; 2017\n    </footer>\n  ",
-            styles: ["\n    .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }\n  "]
-        })
+            template: "\n    <div class=\"jumbotron\">\n      <h1>Welcome to Our App!</h1>\n    </div>\n    <div *ngIf=\"users\">\n      <div *ngFor=\"let user of users\">\n        <h2>{{ user.first_name }}</h2>\n      </div>\n    </div>\n  "
+        }),
+        __metadata("design:paramtypes", [http_1.Http])
     ], AppComponent);
     return AppComponent;
 }());
